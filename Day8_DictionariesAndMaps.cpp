d7b5b9433fb13a37ca50b421175d13ad0582eb16
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-09 16:33:14 +0300

    Day 8 -> Correction

--- Day8_DictionariesAndMaps.cpp
+++ Day8_DictionariesAndMaps.cpp
@@ -9,21 +9,19 @@ using namespace std;
 
 int main() {
     map<string, int> telBook;
-    int input_size_1, input_size_2, input_i;
+    long long input_size, input_i;
     string input_s1, input_s2;
 
-    cin >> input_size_1;
+    cin >> input_size;
 
-    input_size_2 = input_size_1;
-
-    while(input_size_1--){
+    while(input_size--){
         cin >> input_s1 >> input_i;
 
         telBook.insert({input_s1, input_i});
     }
 
-    while(input_size_2--){
-        cin >> input_s2;
+    while(cin >> input_s2){
+
         if(!(telBook.find(input_s2) == telBook.end())){
             cout << input_s2 << "=" << telBook.find(input_s2)->second << endl;
         }
